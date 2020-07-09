219dc13fc7acae16490fbcc8e33aa4ab31feb9c6
Jerry <zhouchao486@gmail.com>
2020-07-09 21:29:15 +0800

    fix bug that can not support GDR

--- pystdf/Writers.py
+++ pystdf/Writers.py
@@ -35,8 +35,8 @@ class TextWriter:
         self.stream = stream
         self.delimiter = delimiter
 
-    @staticmethod
-    def text_format(rectype, field_index, value):
+    # @staticmethod
+    def text_format(self, rectype, field_index, value):
         field_type = rectype.fieldStdfTypes[field_index]
         if value is None:
             return ""
